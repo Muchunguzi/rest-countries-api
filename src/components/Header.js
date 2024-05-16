1920d5296d2ad0fc381a3f305985229aab5033dd
Kennedy muchunguzi <kennedymuchunguzi@gmail.com>
2024-05-16 22:15:57 +0400

    first commmit here

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <p>Where in the World?</p>
+            <span className="space-between"></span>
+             <button className="theme-changer">Dark Mode</button>
+        </div>
+    )
+}
+
+export {Header};
