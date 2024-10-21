9e7be69ade7f3c2a599b0070ab183a5f1b2735c9
Kennedy muchunguzi <kennedymuchunguzi@gmail.com>
2024-10-22 00:59:11 +0400

    added a few files and changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React , {useState, useEffect} from 'react';
+import Search from './components/Search';
 import {FaMoon } from 'react-icons/fa';
 import './App.css';
 
 function App() {
+const [data , setData] = useState([]) //Store JSON data
+
+useEffect(() => {
+  // Fetch the local JSON file
+  fetch('/data.json')
+  .then((response) => response.json())
+  .then((jsonData) => setData(jsonData))
+  .catch((error) => console.error('Error fetching data: ', error));
+}, []); // Empty dependency array to fetch only once when the component mounts
+
+
   return (
     <div className="App">
       < header>
@@ -9,6 +22,7 @@ function App() {
        <p><FaMoon /> Dark Mode</p>
       </header>
       <main_body />
+      <Search data={data} />{/* Pass the fetched data to search component */}
     </div>
   );
 }
