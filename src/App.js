869d45a96583f70a5292fa60b74e9750e0d959c3
Kennedy muchunguzi <kennedymuchunguzi@gmail.com>
2024-11-03 23:04:41 +0400

    small update on the search component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React , {useState, useEffect} from 'react';
-import Search from './components/Search';
-import {FaMoon } from 'react-icons/fa';
-import './App.css';
+import React , {useState , useEffect} from "react";
+import Search from  "./components/Search";
+import {FaMoon} from   "react-icons/fa";
+import "./App.css";
 
-function App() {
-const [data , setData] = useState([]) //Store JSON data
-
-useEffect(() => {
-  // Fetch the local JSON file
-  fetch("http://localhost:3000/rest-countries-api/data.json") 
-  .then((response) => 
-  {
-    console.log(response);
-    return response.json();
-  }
+function App(){
     
-    )
-  .then((jsonData) => {
-    
-    console.log(jsonData) // checking the structure of my data
-    setData(jsonData)})
-  .catch((error) => console.error('Error fetching data: ', error));
-}, []); // Empty dependency array to fetch only once when the component mounts
+  const [data , setData] = useState([]); //Store JSON data
 
-
-  return (
-    <div className="App">
-      < header>
-       <h2>Where in the world ?</h2>
-       <p><FaMoon /> Dark Mode</p>
+  useEffect(() => {
+    //Fetch the local JSON file
+    fetch("http://localhost:3000/rest-countries-api/data.json")
+    .then((response) => response.json())
+    .then((jsonData => {
+      setData(jsonData);
+    }))
+    .catch((error) => console.error("Fetch encountered an error", error));
+  }, []); // Empty dependency array to fetch only once when the component mounts
+   
+   return (
+    <div className = "App">
+      <header>
+        <h1>Where are you from ? </h1>
+        <h2><FaMoon /> Dark Mode</h2>
       </header>
-      <Search data={data} />{/* Pass the fetched data to search component */}
+      <Search data={data}  /> {/*  Pass the fetched data to the search component*/}
     </div>
-  );
+   )
+
+
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,48 +1,49 @@
-import React, {useState} from 'react';
+import React , {useState} from "react";
 
 const Search = ({data}) => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredResults , setFilteredResults] = useState([]);
 
 
+    const [searchTerm , setSearchTerm] = useState("");
+    const [filteredSearchTerm , setFilteredSearchTerm] = useState([]);
 
-    const handleSearch = (event) => {
+    const handleChange = (event) => {
         const value = event.target.value;
         setSearchTerm(value);
-
-        // Filter the data base on the search term
-        if(value.length > 0) { 
-            const results = data.filter((item) =>
-                item.name?.toLowerCase().includes(value.toLowerCase())// Assuming `name` is the property to search in JSON 
-            );
-            setFilteredResults(results);
+        //Filtering the searched terms based on our json data
+        if(value.length > 0){
+            const results = data.filter((item) => 
+                item.name?.toLowerCase().includes(value.toLowerCase())
+                
+            )
+            setFilteredSearchTerm(results);
         } else {
-            setFilteredResults([]);
+            setFilteredSearchTerm([]);
         }
-    };
+    }
+
 
     return (
         <div>
-            <input 
-             type="text"
-             placeholder="Search..."
-             value={searchTerm}
-             onChange={handleSearch}
+            <input
+            
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange = {handleChange}
+            
             />
             <ul>
-                {filteredResults.length > 0 ? (
-                    filteredResults.map((item , index) => (
-                        <li key={index}>{item.name}</li> // Assuming the JSON data has a `name` property
-                    ))
-                ) : (
-                    <li>No results found</li>
-                )}
+            {filteredSearchTerm.length > 0 ? filteredSearchTerm.map((item, index) => (
+                    <li key={index}>
+                    <img src={item.flag} alt="flag for this country" width="30%" /> <br />
+                    {item.name}
+                    </li>
+                )) : <li>Search is inValid!</li>
+            }
             </ul>
         </div>
-    );
+    )
 
+}
 
-
-};
-
-export default  Search ; 
+export default Search;
