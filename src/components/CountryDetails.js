df7f82ae43fcba095538186d40fd767246680de5
Kennedy muchunguzi <kennedymuchunguzi@gmail.com>
2024-12-09 22:16:12 +0400

    added some modifications onto countryDetails component

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -5,37 +5,50 @@ const CountryDetails = ({countryDetails}) => {
 
     return (
         <div className="countryDetails">
-          <div>
-            <table>
-              <tr>
-                <td>
-                <img src={countryDetails.flag} width="100%"/>
-                </td>
-                <td>
-                      
-                   <table>
-                   <h2>{countryDetails.name}</h2>
-                    <tr>
-                      <td>
-                        
-                        <label><strong>Native-Name: </strong>{countryDetails.nativeName}</label><br/>
-                        <label><strong>Population: </strong>{countryDetails.population}</label><br />
-                        <label><strong>Region: </strong>{countryDetails.region}</label><br />
-                        <label><strong>Sub-Region: </strong>{countryDetails.subregion}</label><br />
-                        <label><strong>Capital: </strong>{countryDetails.capital}</label><br />
-                      </td>
-
-                      <td>
-                        <label><strong>Top-Level-Domain: </strong>{countryDetails.topLevelDomain}</label><br/>
-                        <label><strong>Currencies: </strong>{countryDetails.currencies[0].name}</label><br />
-                        <label><strong>Languages: </strong>{countryDetails.languages[0].name}</label><br />
-                      </td>
-                    </tr>
-                   </table>
+          <div className="mainContainer"  style={{display: "flex"}}>
+          
+              <div className="flagContainer">
+              <img src={countryDetails.flag} width="100%"/>
+              </div>
+              <div className="detailsContainer" style={{display: "flex"}}>
+              
+                  
+                  <div className="left" style={{textAlign:"start"}}>
+                  <h2>{countryDetails.name}</h2>
+                  <ul style={{listStyleType: "none"}}>
+                    <p>
+                    <li><strong>Native-Name: </strong>{countryDetails.nativeName}</li>
+                    </p>
+                    <p>
+                    <li><strong>Population: </strong>{countryDetails.population}</li>
+                    </p>
+                    <p>
+                    <li><strong>Region: </strong>{countryDetails.region}</li>
+                    </p>
+                    <p>
+                    <li><strong>Sub-Region: </strong>{countryDetails.subregion}</li>
+                    </p>
+                    <p>
+                    <li><strong>Capital: </strong>{countryDetails.capital}</li>
+                    </p>
+                    
+                  </ul>
                  
-                </td>
-              </tr>
-            </table>
+
+                  </div>
+                  <div className="right" style={{textAlign:"start"}}>
+                  <ul style={{listStyleType: "none"}}>
+                    <li><strong>Top-Level-Domain: </strong>{countryDetails.topLevelDomain}</li>
+                    <li><strong>Currencies: </strong>{countryDetails.currencies[0].name}</li>
+                    <li><strong>Languages: </strong>{countryDetails.languages[0].name}</li>
+                  </ul>
+                  </div>
+             
+                
+                 
+                
+              </div>  
+              
           </div>
           <div>
 
